fd0d4b04274e3973d6649a449d92fb6cf5927fcd
Tanxeem <itx.tanxeem@gmail.com>
2025-05-20 13:18:10 +0300

    setup routes and controllers

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,18 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import authRoutes from './routes/auth.routes.js';
 
 const app = express();
 dotenv.config();
+
+app.use(express.json());
+
+
+app.get('/', (req, res) => {
+    res.send('Welcome to the Leetlabs');
+});
+
+app.use('/api/v1/auth', authRoutes)
 
 app.listen(process.env.PORT, () => {
     console.log(`Server running on port ${process.env.PORT}`);
