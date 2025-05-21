668d3782a6762f404bd43c9bcd5e0c4fcb26bac6
tanxeem <itx.tanxeem@gmail.com>
2025-05-21 23:09:10 +0300

    complete Backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,22 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
 import authRoutes from './routes/auth.routes.js';
 import cookieParser from 'cookie-parser';
 import problemRoutes from './routes/problem.routes.js';
+import executionRoute from './routes/executeCode.routes.js';
+import submissionRoutes from './routes/submission.routes.js';
+import playlistRoutes from './routes/playlist.routes.js';
 
 const app = express();
 dotenv.config();
+
+app.use(
+    cors({
+      origin: "http://localhost:5174",
+      credentials: true,
+    })
+  );
 
 app.use(express.json());
 app.use(cookieParser());
@@ -18,6 +29,9 @@ app.get('/', (req, res) => {
 app.use('/api/v1/auth', authRoutes)
 app.use('/api/v1/problems', problemRoutes)
 app.use("/api/v1/execute-code", executionRoute);
+app.use("/api/v1/submission", submissionRoutes);
+
+app.use("/api/v1/playlist", playlistRoutes);
 
 app.listen(process.env.PORT, () => {
     console.log(`Server running on port ${process.env.PORT}`);
