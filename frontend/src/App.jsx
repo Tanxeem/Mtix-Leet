6408fdfa545ad4ab46a519db4abc4c0410619081
tanxeem <itx.tanxeem@gmail.com>
2025-06-01 23:14:04 +0300

    frontend signup signin and zustand

---
+++ frontend/src/App.jsx
@@ -0,0 +1,21 @@
+import { Routes, Route, Navigate } from "react-router-dom"
+import HomePage from "./page/HomePage"
+import LoginPage from "./page/LoginPage"
+import SignupPage from "./page/SignupPage"
+
+function App() {
+  let authUser = null
+
+  return (
+
+    <div className="flex flex-col items-center justify-start">
+      <Routes>
+        <Route path="/" element={authUser ? <HomePage /> : <Navigate to={"/login"} />} />
+        <Route path="/login" element={!authUser ? <LoginPage /> : <Navigate to={"/"} />} />
+        <Route path="/signup" element={! authUser ? <SignupPage /> : <Navigate to={"/"} />} />
+      </Routes>
+    </div>
+  )
+}
+
+export default App
